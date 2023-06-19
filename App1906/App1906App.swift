65db6ecd6c11d9a81e0d7514d7d7c9c3e2b727a6
Ensara <ensaraaliyah791@gmail.com>
2023-06-19 11:41:41 +0200

    Initial Commit

---
+++ App1906/App1906App.swift
@@ -0,0 +1,17 @@
+//
+//  App1906App.swift
+//  App1906
+//
+//  Created by Ensara Alija on 19.06.23.
+//
+
+import SwiftUI
+
+@main
+struct App1906App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
